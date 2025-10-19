87838081fa593b92686b87e1c8f7283b562e9444
Isabella Clemandot <isabella.clemandot@gmail.com>
2025-10-19 11:09:30 -0500

    First route

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+  const url = 'https://api.hubspot.com/crm/v3/objects/2-174937413?properties=name,email,address';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    const response = await axios.get(url, { headers });
+    const crmRecords = response.data.results;
+    res.render('homepage', { crmRecords });
+  } catch (error) {
+    console.error(error);
+    res.send('Error loading data');
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
